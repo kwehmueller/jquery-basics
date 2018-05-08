18527c501206f0a4e54a10c55086fe49f74fc19e
Kevin Wehmueller <kwehmueller@taoti.com>
2018-05-08 02:27:53 +0000

    Demonstrate basic differences between JavaScript and jQuery syntax

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const box = document.querySelector('.box');
+//box.style.display = 'none';
+
+//jQUery('.box').hide();
+//$('.box').hide();
+//$('.box').show();
+
+/*box.addEventListener('click', function(){
+  alert('You clicked me!');
+});*/
+
+$('.box').click(function(){
+  alert('You clicked me with jQuery!');
+});
